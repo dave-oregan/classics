eba580f4906cec6c4ecbbe3b352cf91effad1186
David <oreagn10@gmail.com>
2022-10-30 18:04:30 -0400

    Update for publish
    -moved in progress game to new branch
    -create home page

--- searchFunctions.js
+++ searchFunctions.js
@@ -1,8 +1,8 @@
 const games = [
     'Meteoroids - v1.2 21 August 2022 arcade shooter space',
     'Extraterrestrials - v1.0 21 August 2022 arcade shooter space',
-    'Pizza Man - v1.0 Day Month 2022 arcade food maze',
-    'Template - v1.0 Day Month Year tag#1 tag#2 tag#3'
+    // 'Pizza Man - v1.0 Day Month 2022 arcade food maze',
+    // 'Template - v1.0 Day Month Year tag#1 tag#2 tag#3'
 ];
 var gamesToShow = [];
 
